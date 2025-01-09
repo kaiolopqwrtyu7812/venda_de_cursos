e97450e693f392f7b35cb261a991dbf55f967a43
kaio <alangoncalvesk@gmail.com>
2025-01-09 13:04:10 -0300

    atulização 3

--- javaScript.js
+++ javaScript.js
@@ -1,50 +1,20 @@
-    // Função para lançar confetes
-    function confetti() {
-        const duration = 1 * 1000;
-        const end = Date.now() + duration;
-        (function frame() {
-            confetti({
-                particleCount: 3,
-                angle: 60,
-                spread: 55,
-                origin: { x: 0 },
-                colors: ['#ff0', '#0f0', '#00f'],
-            });
-            confetti({
-                particleCount: 3,
-                angle: 120,
-                spread: 55,
-                origin: { x: 1 },
-                colors: ['#ff0', '#0f0', '#00f'],
-            });
-            if (Date.now() < end) {
-                requestAnimationFrame(frame);
-            }
-        })();
-    }
-    // Chamar a função confetti ao carregar a página
-    window.onload = confetti;
+const btn = document.getElementById('button');
 
-    // Configurar EmailJS
-    emailjs.init("xnsr-iX_51ACpSxlJ");
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
 
-    // Função para enviar o formulário
-    const btn = document.getElementById('button');
+   btn.value = 'Sending...';
 
-    document.getElementById('form').addEventListener('submit', function(event) {
-        event.preventDefault();
+   const serviceID = 'default_service';
+   const templateID = 'template_cu771jc';
 
-        btn.value = 'Enviando...';
-
-        const serviceID = 'default_service';
-        const templateID = 'template_cu771jc';
-
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                btn.value = 'Enviar Email';
-                alert('Mensagem enviada com sucesso!');
-            }, (err) => {
-                btn.value = 'Enviar Email';
-                alert(JSON.stringify(err));
-            });
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
     });
+});
